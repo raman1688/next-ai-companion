010704d61c623752bc3a7257c5a69cc1414ed0c7
Ramandeep Malhotra <r.malhotra@adform.com>
2023-08-15 17:02:56 +0530

    added search filter component

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,4 +1,56 @@
 "use client";
+
+import { ChangeEventHandler, useEffect, useState } from "react";
+
+import { Search } from "lucide-react";
+import qs from "query-string";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import { Input } from "@/components/ui/input";
+import { useDebounce } from "@/hooks/use-debounce";
+
 export default function SearchInput() {
-  return <div>SearchInput</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const categoryId = searchParams.get("categoryId");
+  const name = searchParams.get("name");
+
+  const [value, setvalue] = useState(name || "");
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setvalue(e.target.value);
+  };
+  useEffect(() => {
+    const query = {
+      name: debouncedValue,
+      categoryId,
+    };
+
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      {
+        skipEmptyString: true,
+        skipNull: true,
+      }
+    );
+
+    router.push(url);
+  }, [debouncedValue, router, categoryId]);
+
+  return (
+    <div className="relative">
+      <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
+      <Input
+        placeholder="Search..."
+        className="pl-10 bg-primary/10"
+        onChange={onChange}
+        value={value}
+      />
+    </div>
+  );
 }
